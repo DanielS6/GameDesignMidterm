0f64c72b2ae3d8c4c73ab5941f1a6e1fcfec7c78
DanielS6 <Daniel@nevet.com>
2022-02-21 16:58:18 -0500

    Add preliminary flower object
    Currently displays as a purple capsule, pending creation of proper assets
    
    Every 5 seconds, it creates a new nectar instance (for now the "PickUp"
    game object) somewhere nearby, for the player (bee) to try and capture.
    This replaces the 16 initial instances in the scene, which are removed.
    
    Additionally, when the player collides with a nectar, destroy it instead of
    hiding it.

---
+++ bbkeeper/Assets/Scripts/Flower.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour {
+
+    // Prefab of nectar to create, for now just a placeholder
+    public GameObject nectarPrefab;
+
+    // Timing the random nectar creation, for now a nectar is created every
+    // 5 seconds
+    // public float nectarMaxDelay = 2.0f;
+    private float nectarTimer;
+
+    // Directions around the flower to create new nectar, multiplied by a
+    // random magnitute
+    private Vector3[] creationDirections = new Vector3[8];
+
+    // Initialize creationPoints on startup
+    void Start() {
+        creationDirections[0] = new Vector3(0.0f, 0.0f, 1.0f);
+        creationDirections[1] = new Vector3(1.0f, 0.0f, 1.0f);
+        creationDirections[2] = new Vector3(1.0f, 0.0f, 0.0f);
+        creationDirections[3] = new Vector3(1.0f, 0.0f, -1.0f);
+        creationDirections[4] = new Vector3(0.0f, 0.0f, -1.0f);
+        creationDirections[5] = new Vector3(-1.0f, 0.0f, -1.0f);
+        creationDirections[6] = new Vector3(-1.0f, 0.0f, 0.0f);
+        creationDirections[7] = new Vector3(-1.0f, 0.0f, 1.0f);
+    }
+
+    void FixedUpdate() {
+        // float timeToCreate = Random.Range(0, nectarMaxDelay);
+        nectarTimer += 0.01f;
+        // for now a nectar is created every 5 seconds
+        if (nectarTimer > 5.0f) {
+            createNewNectar();
+            nectarTimer = 0f;
+        }
+    }
+
+    void createNewNectar() {
+        // Create a new nectar instance around the current flower
+        // Will be in one of the directions around the flower
+        int creationDirNum = Random.Range(0, 7);
+        Vector3 creationDirection = creationDirections[creationDirNum];
+        float creationAmplitude = Random.Range(1.0f, 3.0f);
+        Instantiate(
+            nectarPrefab,
+            transform.position + (creationDirection * creationAmplitude),
+            Quaternion.identity // no rotation
+        );
+    }
+}
--- bbkeeper/Assets/Scripts/PlayerController.cs
+++ bbkeeper/Assets/Scripts/PlayerController.cs
@@ -51,7 +51,7 @@ public class PlayerController : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
         if (other.gameObject.CompareTag("PickUp")) {
-            other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
             count += 1;
             SetCountText();
         }
