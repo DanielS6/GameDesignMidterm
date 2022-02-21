4a6be82b10df14065e17a94345f6da7f391b9cad
DanielS6 <Daniel@nevet.com>
2022-02-21 16:01:07 -0500

    PlayerController.cs tweaks, adjust win text location
    PlayerController.cs: style tweaks, add some documentation, clean up the layout
    a bit. No function changes
    
    For the win text, move it down a bit so that it is fully within the view of
    the player.

--- bbkeeper/Assets/Scripts/PlayerController.cs
+++ bbkeeper/Assets/Scripts/PlayerController.cs
@@ -4,61 +4,56 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
+    // Movement
     public float speed = 0;
-    public TextMeshProUGUI countText;
-    public GameObject winTextObject;
-
-    private Rigidbody rb;
-    private int count; 
-
     private float movementX;
     private float movementY;
 
+    // User display
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    // Actual moving object
+    private Rigidbody playerRigidBody;
+
+    // Number of collected items so far
+    private int count;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
+    void Start() {
+        playerRigidBody = GetComponent<Rigidbody>();
         count = 0;
 
         SetCountText();
         winTextObject.SetActive(false);
-
     }
 
-    void OnMove(InputValue movementValue)
-    {
+    void OnMove(InputValue movementValue) {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
 
-    void SetCountText()
-    {
+    void FixedUpdate() {
+        // Move the player according to current direction and speed
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        playerRigidBody.AddForce(movement * speed);
+    }
+
+    void SetCountText() {
         countText.text = "Count: " + count.ToString();
-        if (count >= 16){
+        if (count >= 16) {
             winTextObject.SetActive(true);
         }
-
     }
 
-    void FixedUpdate() 
-    {
-        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-
-        rb.AddForce(movement * speed);
-    }
-
-    private void OnTriggerEnter(Collider other) 
-    {
-        if (other.gameObject.CompareTag("PickUp")){
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("PickUp")) {
             other.gameObject.SetActive(false);
             count += 1;
+            SetCountText();
         }
-        
-        SetCountText();
     }
 }
