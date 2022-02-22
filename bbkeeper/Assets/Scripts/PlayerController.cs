6b68cdb6216c51934a0013515ab05e38ec5468b3
DanielS6 <Daniel@nevet.com>
2022-02-21 21:49:54 -0500

    Bee: look in the direction of the last arrow press
    Whenever the movement is updated, if currently moving (i.e. not from letting go
    of an arrow) have the bee face the direction that was last pressed. This is not
    exactly the same as the direction of travel, since the bee will maintain any
    momentum, but is reflective of the direction the player wants to move the bee.

--- bbkeeper/Assets/Scripts/PlayerController.cs
+++ bbkeeper/Assets/Scripts/PlayerController.cs
@@ -34,6 +34,30 @@ public class PlayerController : MonoBehaviour {
         Vector2 movementVector = movementValue.Get<Vector2>();
         movementX = movementVector.x;
         movementY = movementVector.y;
+        // Currently pressing a move key, rather than letting go
+        if (movementX != 0 || movementY != 0) {
+            updateLookDirection(movementX, movementY);
+        }
+    }
+
+    private void updateLookDirection(float lookX, float lookY) {
+        // For each direction, the looX, lookY, and desired rotation are:
+        // Up arrow: 0, 1, 90
+        // down arrow: 0, -1, -90
+        // right arrow: 1, 0, 180
+        // left arrow: -1, 0, 0
+        float yRotation;
+        if (lookX == 0) {
+            // up or down
+            yRotation = (lookY == 1 ? 90.0f : -90.0f);
+        } else {
+            // left or right
+            yRotation = (lookX == 1 ? 180.0f : 0.0f);
+        }
+        // reset, so that the rotation is relative to starting 0/0/0 instead
+        // of relative to the current orientation
+        transform.localRotation = Quaternion.identity;
+        transform.Rotate(0.0f, yRotation, 0.0f, Space.World);
     }
 
     void FixedUpdate() {
