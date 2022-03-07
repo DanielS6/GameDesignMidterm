f08101edeb3729cfb8bc09d3e4243594fa5d0e66
Catherine Ding <catherineding28@gmail.com>
2022-03-06 19:46:36 -0500

    completed scene management logic

--- bbkeeper/Assets/Scripts/SceneController.cs
+++ bbkeeper/Assets/Scripts/SceneController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartingController : MonoBehaviour {
+public class SceneController : MonoBehaviour {
 
     // Event handler
     public void LaunchGameScene() {
         SceneManager.LoadScene("GameScene");
+    }
+
+    public void LaunchEndGame() {
+        SceneManager.LoadScene("EndScene");
     }
 
 }
--- bbkeeper/Assets/Scripts/timer.cs
+++ bbkeeper/Assets/Scripts/timer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -28,6 +29,8 @@ public class Timer : MonoBehaviour
                 Debug.Log("Time has run out!");
                 timeRemaining = 0;
                 timerIsRunning = false;
+
+                SceneManager.LoadScene("ScoreScene");
             }
         }
     }
